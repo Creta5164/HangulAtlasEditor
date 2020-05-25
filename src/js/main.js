d0ee1499257b6d909c4605988932e3ab9078d076
Creta Park <creta5164@gmail.com>
2020-05-26 03:11:26 +0900

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+let HangulAtlasEditor = null;
+
+(function() {
+    
+    HangulAtlasEditor = function() {
+        
+    };
+    
+    window.addEventListener('load', HangulAtlasEditor.bind(HangulAtlasEditor) );
+    
+    HangulAtlasEditor.DrawGlyph = function(x, y, glyph) {
+        
+    };
+    
+    HangulAtlasEditor.GenerateFnt = function() {
+        
+        
+    };
+    
+})();
